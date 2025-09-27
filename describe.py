f39eeadd0d9747bf50b04eb6365b77ad5763e0a2
Alina <allinne@gmail.com>
2025-09-27 21:23:27 +0200

    Describe five pics
    - Load model once, process many pics
    - Timestamps

--- describe.py
+++ describe.py
@@ -1,4 +1,6 @@
 
+
+import datetime
 # Install:
 # pip install torch torchvision transformers pillow huggingface_hub
 from transformers import BlipProcessor, BlipForConditionalGeneration
@@ -7,18 +9,26 @@ import torch
 
 # Download:
 # huggingface-cli download Salesforce/blip-image-captioning-base --local-dir ./models/blip-base
-model_id = "./models/blip-base"
+model_path = './models/blip-base'
+
+def L(s):
+     print(str(datetime.datetime.utcnow()) + ' ' + s)
 
 # Load processor and model into VRAM
-processor = BlipProcessor.from_pretrained(model_id)
-model = BlipForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16, device_map="auto")
+L('Starting')
+processor = BlipProcessor.from_pretrained(model_path)
+model = BlipForConditionalGeneration.from_pretrained(model_path, torch_dtype=torch.float16, device_map='auto')
+L('Loaded')
 
-image_path = "test/575373896.jpg"
-image = Image.open(image_path).convert("RGB")
-inputs = processor(images=image, return_tensors="pt").to(model.device)
+pics = ('575373896.jpg', '575374562.jpg', '583560095.jpg', '585683353.jpg', '594877746.jpg')
+for p in pics:
+    L(f'Processing {p}')
+    img = Image.open('test/' + p).convert('RGB')
+    inputs = processor(images=img, return_tensors='pt').to(model.device)
 
-with torch.no_grad():
-    output_ids = model.generate(**inputs, max_new_tokens=30)
-    caption = processor.decode(output_ids[0], skip_special_tokens=True)
+    with torch.no_grad():
+        ids = model.generate(**inputs, max_new_tokens=30)
+        caption = processor.decode(ids[0], skip_special_tokens=True)
+    L(f'Description: {caption}')
 
-print("Generated caption:", caption)
+L('Done')
